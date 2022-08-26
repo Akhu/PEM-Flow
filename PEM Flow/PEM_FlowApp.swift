f9df63b22b007ae2174086ce456760bc574bdbe2
Anthony Da Cruz <me@anthony-dacruz.com>
2022-08-26 10:36:18 +0200

    Initial Commit

---
+++ PEM Flow/PEM_FlowApp.swift
@@ -0,0 +1,20 @@
+//
+//  PEM_FlowApp.swift
+//  PEM Flow
+//
+//  Created by Anthony Da cruz on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PEM_FlowApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
